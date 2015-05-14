4d8b0bb9dee6d606953ead427d524830eb5fff70
fdaciuk <f.daciuk@gmail.com>
2015-05-14 13:15:55 -0300

    Add gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+'use strict';
+
+var gulp = require( 'gulp' );
+var karma = require( 'karma' ).server;
+var exec = require( 'child_process' ).exec;
+
+gulp.task( 'test', function( done ) {
+  karma.start({
+    configFile: __dirname + '/karma.conf.js',
+    singleRun: true
+  }, done );
+});
+
+gulp.task( 'webserver', function() {
+  exec( 'python -m SimpleHTTPServer 9001' );
+  console.log( 'Server listen on port 9001' );
+});
+
+gulp.task( 'watch', [ 'test' ], function() {
+  gulp.watch( '{test,src}/**/*.js', [ 'test' ]);
+});
+
+gulp.task( 'default', [ 'webserver', 'watch' ]);
